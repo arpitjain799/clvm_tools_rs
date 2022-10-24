b1b3dc9d422efe0a2bd0af82433d71dcdbb342b2
arty <art.yerkes@gmail.com>
2022-10-24 05:14:41 -0700

    Better (still imperfect).  The ChiaOutcome object is mostly either, and provides an easy monadic expereience.  We'll model everything involving runtime execution using do blocks and (<-) bind so we wind up modelling exceptions properly.  We're producing in many cases properly represented programs, and now need typesynth to ensure purescript doesn't get hung up on ambiguity which is ok generally.  This will involve flattening existentials into forall variables and stating them in the function signature.

--- src/tests/compiler/types/purescript.rs
+++ src/tests/compiler/types/purescript.rs
@@ -14,9 +14,10 @@ use std::rc::Rc;
 use crate::classic::clvm::__type_compatibility__::{bi_one, bi_zero};
 use crate::compiler::compiler::DefaultCompilerOpts;
 use crate::compiler::comptypes::{BodyForm, CompileForm, CompilerOpts, HelperForm};
-use crate::compiler::sexp::{SExp, decode_string};
+use crate::compiler::evaluate::dequote;
 use crate::compiler::frontend::frontend;
 use crate::compiler::fuzzer::FuzzProgram;
+use crate::compiler::sexp::{SExp, decode_string};
 use crate::compiler::typechia::type_level_macro_transform;
 use crate::util::Number;
 
@@ -25,118 +26,186 @@ lazy_static! {
 module Main where
 
 import Prelude
+import Data.Maybe
+import Data.Tuple
 import Effect (Effect)
 import Effect.Console (log)
 
-data Atom = Atom
-data Atom32 = Atom32
-data Nullable a = EmptyNullable
-data Pair a b = Pair
-data Exec x = Exec
-data ChiaFun a b = ChiaFun
-data Any = Any
+data ChiaPrim = ChiaAtom String | ChiaCons ChiaPrim ChiaPrim
 
-class Default x where
-  default :: x
+{- These are type level representations, but the actual data is a chia value
+ - This simulates the conversion of the program from typed to untyped.
+ -}
+data Nil = Nil ChiaPrim
+data Atom = Atom ChiaPrim
+data Atom32 = Atom32 ChiaPrim
+data Nullable a = Nullable ChiaPrim
+data Pair a b = Pair ChiaPrim
+data Exec x = Exec ChiaPrim
+data Any = Any ChiaPrim
+data ChiaFun a b = ChiaFun ChiaPrim
 
-instance defaultAtom :: Default Atom where
-  default = Atom
+-- Monad for chia
+data ChiaOutcome t = ChiaResult t | ChiaException ChiaPrim
 
-instance defaultAtom32 :: Default Atom32 where
-  default = Atom32
+instance chiaOutcomeFunctor :: Functor ChiaOutcome where
+  map :: forall a b. (a -> b) -> ChiaOutcome a -> ChiaOutcome b
+  map f (ChiaResult v) = ChiaResult $ (f v)
+  map _ (ChiaException e) = ChiaException e
 
-instance defaultNullableA :: (Default a) => Default (Nullable a) where
-  default = EmptyNullable
+class HasValue t where
+  getValue :: t -> ChiaPrim
 
-instance defaultPairAB :: (Default a, Default b) => Default (Pair a b) where
-  default = Pair
+instance chiaPrimHasValue :: HasValue ChiaPrim where
+  getValue p = p
 
-instance defaultExecX :: (Default x) => Default (Exec x) where
-  default = Exec
+instance chiaNilHasValue :: HasValue Nil where
+  getValue (Nil v) = v
 
-instance defaultChiaFunAB :: (Default a, Default b) => Default (ChiaFun a b) where
-  default = ChiaFun
+instance chiaAtomHasValue :: HasValue Atom where
+  getValue (Atom v) = v
 
-instance defaultAny :: Default Any where
-  default = Any
+instance chiaAtom32HasValue :: HasValue Atom32 where
+  getValue (Atom32 v) = v
+
+instance chiaNullableHasValue :: HasValue (Nullable a) where
+  getValue (Nullable v) = v
+
+instance chiaPairHasValue :: HasValue (Pair a b) where
+  getValue (Pair v) = v
+
+instance chiaExecHasValue :: HasValue (Exec x) where
+  getValue (Exec v) = v
+
+instance chiaAnyHasValue :: HasValue Any where
+  getValue (Any v) = v
+
+instance chiaFunHasValue :: HasValue (ChiaFun a b) where
+  getValue (ChiaFun v) = v
+
+class FromValue t where
+  fromValue :: ChiaPrim -> t
+
+instance chiaPrimFromValue :: FromValue ChiaPrim where
+  fromValue p = p
+
+instance chiaNilFromValue :: FromValue Nil where
+  fromValue p = Nil p
+
+instance chiaAtomFromValue :: FromValue Atom where
+  fromValue p = Atom p
+
+instance chiaAtom32FromValue :: FromValue Atom32 where
+  fromValue p = Atom32 p
+
+instance chiaNullableFromValue :: FromValue (Nullable a) where
+  fromValue p = Nullable p
+
+instance chiaPairFromValue :: FromValue (Pair a b) where
+  fromValue p = Pair p
+
+instance chiaExecFromValue :: FromValue (Exec x) where
+  fromValue p = Exec p
+
+instance chiaAnyFromValue :: FromValue Any where
+  fromValue p = Any p
+
+instance chiaFunFromValue :: FromValue (ChiaFun a b) where
+  fromValue p = ChiaFun p
 
 -- The following constraints are used to implement the basic subtype ladder.
 class Convert x y where
   cvt :: x -> y
 
-instance cvtUnitUnit :: Convert Unit Unit where
-  cvt _ = unit
+instance cvtNilNil :: Convert Nil Nil where
+  cvt (Nil x) = Nil x
 
-instance cvtAnyUnit :: Convert Any Unit where
-  cvt _ = unit
+instance cvtAnyNil :: Convert Any Nil where
+  cvt (Any x) = Nil x
 
-instance cvtNullableXUnit :: Convert (Nullable x) Unit where
-  cvt _ = unit
+instance cvtNullableXNil :: Convert (Nullable x) Nil where
+  cvt (Nullable x) = Nil x
 
-instance cvtXNullableX :: Convert x (Nullable x) where
-  cvt x = EmptyNullable
+instance cvtXNullableX :: (HasValue x) => Convert x (Nullable x) where
+  cvt x = Nullable (getValue x)
 
-instance cvtUnitAny :: Convert Unit Any where
-  cvt _ = Any
+instance cvtNilAny :: Convert Nil Any where
+  cvt (Nil x) = Any x
 
 instance cvtAtomAny :: Convert Atom Any where
-  cvt _ = Any
+  cvt (Atom x) = Any x
 
 instance cvtAtom32Any :: Convert Atom32 Any where
-  cvt _ = Any
+  cvt (Atom32 x) = Any x
 
 instance cvtNullableXAny :: (Convert x Any) => Convert (Nullable x) Any where
-  cvt _ = Any
+  cvt (Nullable x) = Any x
 
 instance cvtPairABAny :: (Convert a Any, Convert b Any) => Convert (Pair a b) Any where
-  cvt Pair = Any
+  cvt (Pair x) = Any x
 
 instance cvtAnyAny :: Convert Any Any where
-  cvt _ = Any
+  cvt (Any x) = Any x
 
 instance cvtAtomAtom :: Convert Atom Atom where
-  cvt Atom = Atom
+  cvt (Atom x) = Atom x
 
 instance cvtAtom32Atom :: Convert Atom32 Atom where
-  cvt Atom32 = Atom
+  cvt (Atom32 x) = Atom x
 
-instance cvtUnitAtom :: Convert Unit Atom where
-  cvt _ = Atom
+instance cvtNilAtom :: Convert Nil Atom where
+  cvt (Nil x) = Atom x
 
 instance cvtAnyAtom :: Convert Any Atom where
-  cvt _ = Atom
+  cvt (Any x) = Atom x
 
 instance cvtAtom32Atom32 :: Convert Atom32 Atom32 where
-  cvt Atom32 = Atom32
+  cvt (Atom32 x) = Atom32 x
 
 instance cvtAnyAtom32 :: Convert Any Atom32 where
-  cvt _ = Atom32
+  cvt (Any x) = Atom32 x
 
 instance cvtPairABPairXY :: (Convert a x, Convert b y) => Convert (Pair a b) (Pair x y) where
-  cvt Pair = Pair
+  cvt (Pair x) = Pair x
 
 instance cvtAnyPairAB :: (Convert Any a, Convert Any b) => Convert Any (Pair a b) where
-  cvt x = Pair
+  cvt (Any x) = Pair x
 
 instance cvtAnyExecX :: (Convert Any x) => Convert Any (Exec x) where
-  cvt x = Exec
+  cvt (Any x) = Exec x
 
 instance cvtAnyChiaFunAB :: Convert Any (ChiaFun a b) where
-  cvt _ = ChiaFun
+  cvt (Any x) = ChiaFun x
 
-newtype List a = L1 (Nullable a)
+class ListOfAtoms x where
+  unroll :: x -> Maybe (Tuple Atom x)
 
-instance cvtUnitList :: (Convert Any a) => Convert Unit (List a) where
-  cvt _ = L1 EmptyNullable
+instance listOfAtomsNil :: ListOfAtoms Nil where
+  unroll (Nil x) = Nothing
 
-instance cvtPairABList :: (Convert b (List a)) => Convert (Pair a b) (List a) where
-  cvt _ = L1 EmptyNullable
+instance listOfAtomsNullable :: (ListOfAtoms x, FromValue x) => ListOfAtoms (Nullable x) where
+  unroll (Nullable v) = unroll (fromValue v)
 
-sha256 :: List Atom -> Atom32
-sha256 _ = Atom32
+instance listOfAtomsPair :: (FromValue b, ListOfAtoms b) => ListOfAtoms (Pair a b) where
+  unroll (Pair (ChiaCons x y)) = Just (Tuple (fromValue x) (fromValue y))
+  unroll (Pair _) = Nothing
 
-f :: forall f0 r0. (Default f0) => (Pair (Pair f0 r0) Unit) -> f0
-f Pair = default
+sha256 :: forall x. (ListOfAtoms x) => x -> ChiaOutcome Atom32
+sha256 _ = ChiaResult $ Atom32 (ChiaAtom \"\")
+
+multiply :: forall x. (ListOfAtoms x) => x -> ChiaOutcome Atom
+multiply _ = ChiaResult $ Atom (ChiaAtom \"\")
+
+subtract :: Pair Atom (Pair Atom Unit) -> ChiaOutcome Atom
+subtract _ = ChiaResult $ Atom (ChiaAtom \"\")
+
+f :: forall f0 r0. (FromValue f0) => (Pair (Pair f0 r0) Nil) -> ChiaOutcome f0
+f (Pair (ChiaCons (ChiaCons a b) _)) = ChiaResult (fromValue a)
+f x = ChiaException (getValue x)
+
+truthy :: forall x. (Convert x Any) => x -> Boolean
+truthy _ = false
+
 "}.to_string();
 
     pub static ref PURESCRIPT_SUFFIX: String = indoc!{"
@@ -149,6 +218,9 @@ main = do
         let mut op_dict = HashMap::new();
 
         op_dict.insert(b"-".to_vec(), b"subtract".to_vec());
+        op_dict.insert(b"*".to_vec(), b"multiply".to_vec());
+        op_dict.insert(b"sha256".to_vec(), b"sha256".to_vec());
+        op_dict.insert(b"c".to_vec(), b"c".to_vec());
 
         op_dict
     };
@@ -195,13 +267,15 @@ fn find_callable(prog: &CompileForm, name: &[u8]) -> Option<HelperForm> {
 }
 
 fn produce_body(opts: Rc<dyn CompilerOpts>, result_vec: &mut Vec<String>, prog: &CompileForm, indent: usize, body: &BodyForm) {
-    eprintln!("-- produce_body {}", body.to_sexp());
+    result_vec.push(format!("-- produce_body {}", body.to_sexp()));
     match body {
         BodyForm::Let(_, _, bindings, letbody) => {
+            result_vec.push(format!("{}do", do_indent(indent)));
             for b in bindings.iter() {
-                result_vec.push(format!("{}{} <-", do_indent(indent), decode_string(&b.name)));
-                produce_body(opts.clone(), result_vec, prog, indent + 2, letbody.borrow());
+                result_vec.push(format!("{}{} <-", do_indent(indent + 2), decode_string(&b.name)));
+                produce_body(opts.clone(), result_vec, prog, indent + 4, b.body.borrow());
             }
+            produce_body(opts.clone(), result_vec, prog, indent + 2, letbody.borrow());
         },
         BodyForm::Call(_, elts) => {
             if elts.is_empty() {
@@ -210,13 +284,16 @@ fn produce_body(opts: Rc<dyn CompilerOpts>, result_vec: &mut Vec<String>, prog: 
             }
 
             if let BodyForm::Value(SExp::Atom(_,n)) = elts[0].borrow() {
-                eprintln!("call {}", decode_string(&n));
                 if let Some(callable) = find_callable(prog, n) {
-                    eprintln!("callable {}", callable.to_sexp());
-                    if let HelperForm::Defun(_, _, _, _, _, _) = callable {
+                    if let HelperForm::Defun(_, defname, _, _, _, _) = callable {
+                        result_vec.push(format!("{}do", do_indent(indent)));
                         for (i,a) in elts.iter().skip(1).enumerate() {
-                            result_vec.push(format!("{}arg_{} <-", do_indent(indent), i));
-                            produce_body(opts.clone(), result_vec, prog, indent + 2, a);
+                            result_vec.push(format!("{}farg_{} <-", do_indent(indent + 2), i));
+                            produce_body(opts.clone(), result_vec, prog, indent + 4, a);
+                        }
+                        result_vec.push(format!("{}cvt $ {} $", do_indent(indent + 2), decode_string(&defname)));
+                        for (i,_) in elts.iter().skip(1).enumerate() {
+                            result_vec.push(format!("{}farg_{}", do_indent(indent + 4), i));
                         }
                     } else if let HelperForm::Defmacro(loc, name, macargs, macbody) = callable {
                         // expand macro as in type synthesis.
@@ -226,7 +303,9 @@ fn produce_body(opts: Rc<dyn CompilerOpts>, result_vec: &mut Vec<String>, prog: 
                             loc.clone(),
                             &elts
                         ).unwrap();
-                        produce_body(opts, result_vec, prog, indent, expanded_expression.borrow());
+                        let unquoted_expr = dequote(loc.clone(), expanded_expression).unwrap();
+                        let reparsed = frontend(opts.clone(), vec![unquoted_expr]).unwrap();
+                        produce_body(opts, result_vec, prog, indent, &reparsed.exp);
                     } else {
                         todo!()
                     }
@@ -238,30 +317,25 @@ fn produce_body(opts: Rc<dyn CompilerOpts>, result_vec: &mut Vec<String>, prog: 
             }
         },
         BodyForm::Quoted(SExp::Cons(_,a,b)) => {
-            result_vec.push(format!("{}(cvt (Pair", do_indent(indent)));
-            let borrowed_a: &SExp = a.borrow();
-            let borrowed_b: &SExp = b.borrow();
-            produce_body(opts.clone(), result_vec, prog, indent + 2, &BodyForm::Quoted(borrowed_a.clone()));
-            produce_body(opts, result_vec, prog, indent + 2, &BodyForm::Quoted(borrowed_b.clone()));
-            result_vec.push(format!("{}  )", do_indent(indent)));
+            result_vec.push(format!("{}pure $ (cvt $ Pair $ ChiaAtom \"\")", do_indent(indent)));
         },
         BodyForm::Quoted(SExp::Nil(_)) => {
-            result_vec.push(format!("{}(cvt unit)", do_indent(indent)));
+            result_vec.push(format!("{}pure $ (cvt $ Nil $ ChiaAtom \"\")", do_indent(indent)));
         },
         BodyForm::Quoted(_) => {
-            result_vec.push(format!("{}(cvt Atom)", do_indent(indent)));
+            result_vec.push(format!("{}pure $ (cvt $ Atom $ ChiaAtom \"\")", do_indent(indent)));
         },
         BodyForm::Value(SExp::Nil(_)) => {
-            result_vec.push(format!("{}(cvt unit)", do_indent(indent)));
+            result_vec.push(format!("{}pure $ (cvt $ Nil $ ChiaAtom \"\")", do_indent(indent)));
         },
         BodyForm::Value(SExp::Cons(_,_,_)) => {
-            result_vec.push(format!("{}(cvt Pair)", do_indent(indent)));
+            result_vec.push(format!("{}pure $ (cvt $ Pair $ ChiaAtom \"\")", do_indent(indent)));
         },
         BodyForm::Value(SExp::Atom(_,n)) => {
-            result_vec.push(format!("{}{}", do_indent(indent), decode_string(&n)));
+            result_vec.push(format!("{}pure $ (cvt $ Atom $ ChiaAtom \"\")", do_indent(indent)));
         },
         BodyForm::Value(_) => {
-            result_vec.push(format!("{}(cvt Atom)", do_indent(indent)));
+            result_vec.push(format!("{}pure $ (cvt $ Atom $ ChiaAtom \"\")", do_indent(indent)));
         }
     }
 }
@@ -313,11 +387,14 @@ pub fn chialisp_to_purescript(opts: Rc<dyn CompilerOpts>, prog: &CompileForm) ->
             result_vec.push(format!("{} args = do", name));
 
             let args = collect_args(defargs.clone());
+            result_vec.push("-- produce args".to_string());
             if !args.is_empty() {
                 for (path, a) in args.iter() {
                     result_vec.push(format!("  {} <- {}", decode_string(&a), choose_path(path.clone(), bi_one(), "args".to_string())));
                 }
             }
+
+            result_vec.push(format!("-- produce body for {}", name));
             produce_body(opts.clone(), &mut result_vec, prog, 2, defbody.borrow());
         }
     }
