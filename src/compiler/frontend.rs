883e381ee87615f5f74f5e29526666755bff7291
arty <art.yerkes@gmail.com>
2023-01-10 01:17:20 -0800

    Lambda with capture

--- src/compiler/frontend.rs
+++ src/compiler/frontend.rs
@@ -4,10 +4,12 @@ use std::collections::HashSet;
 use std::rc::Rc;
 
 use crate::classic::clvm::__type_compatibility__::bi_one;
+use crate::compiler::clvm::truthy;
 use crate::compiler::comptypes::{
     list_to_cons, Binding, BodyForm, CompileErr, CompileForm, CompilerOpts, DefconstData,
     DefmacData, DefunData, HelperForm, IncludeDesc, LetData, LetFormKind, ModAccum,
 };
+use crate::compiler::evaluate::make_operator2;
 use crate::compiler::preprocessor::preprocess;
 use crate::compiler::rename::rename_children_compileform;
 use crate::compiler::sexp::{enlist, SExp};
@@ -242,6 +244,115 @@ fn make_let_bindings(
     }
 }
 
+fn make_captures(opts: Rc<dyn CompilerOpts>, sexp: Rc<SExp>) -> Result<Rc<BodyForm>, CompileErr> {
+    if let SExp::Cons(l, f, r) = sexp.borrow() {
+        Ok(Rc::new(make_operator2(
+            &l,
+            "c".to_string(),
+            make_captures(opts.clone(), f.clone())?,
+            make_captures(opts, r.clone())?
+        )))
+    } else if !truthy(sexp.clone()) {
+        Ok(Rc::new(BodyForm::Quoted(SExp::Nil(sexp.loc()))))
+    } else {
+        Ok(Rc::new(compile_bodyform(opts, sexp)?))
+    }
+}
+
+fn find_and_compose_captures(opts: Rc<dyn CompilerOpts>, sexp: &SExp) -> Result<(Rc<SExp>, Option<Rc<BodyForm>>), CompileErr> {
+    eprintln!("find_and_compose {}", sexp);
+    if let SExp::Cons(cl, l, r) = sexp {
+        if let SExp::Cons(_, head, rest) = l.borrow() {
+            if let SExp::Atom(al, name) = head.borrow() {
+                if name == b"=" {
+                    let captures = make_captures(opts, rest.clone())?;
+                    return Ok((Rc::new(SExp::Cons(
+                        cl.clone(),
+                        rest.clone(),
+                        r.clone()
+                    )), Some(captures)));
+                }
+            }
+        }
+    }
+
+    Ok((Rc::new(sexp.clone()), None))
+}
+
+//
+// Lambda
+//
+// (lambda ((= captures) arguments)
+//   (body)
+//   )
+//
+// Yields:
+//
+// M = (mod ((captures) arguments) (body))
+// (qq (a (unquote M) (c (unquote captures) @)))
+fn handle_lambda(opts: Rc<dyn CompilerOpts>, v: &[SExp]) -> Result<BodyForm, CompileErr> {
+    let (args, captures) =
+        find_and_compose_captures(opts.clone(), &v[0])?;
+
+    let mod_form_data =
+        Rc::new(SExp::Cons(
+            v[0].loc(),
+            Rc::new(SExp::atom_from_string(v[0].loc(), "mod")),
+            Rc::new(SExp::Cons(
+                args.loc(),
+                args.clone(),
+                Rc::new(SExp::Cons(
+                    v[1].loc(),
+                    Rc::new(v[1].clone()),
+                    Rc::new(SExp::Nil(v[1].loc()))
+                ))
+            ))
+        ));
+
+    if let Some(captures) = &captures {
+        // Requires captures
+        let subparse = frontend(opts, &[mod_form_data])?;
+        let module = BodyForm::Mod(v[0].loc(), subparse);
+
+        eprintln!("captures {}", captures.to_sexp());
+        let lambda_output =
+            BodyForm::Call(
+                v[0].loc(),
+                vec![
+                    Rc::new(BodyForm::Value(SExp::atom_from_string(v[0].loc(), "list"))),
+                    Rc::new(BodyForm::Quoted(SExp::Atom(v[0].loc(), vec![2]))),
+                    Rc::new(make_operator2(
+                        &v[0].loc(),
+                        "c".to_string(),
+                        Rc::new(BodyForm::Quoted(SExp::Atom(v[0].loc(), vec![1]))),
+                        Rc::new(module)
+                    )),
+                    Rc::new(BodyForm::Call(
+                        v[0].loc(),
+                        vec![
+                            Rc::new(BodyForm::Value(SExp::atom_from_string(v[0].loc(), "list"))),
+                            Rc::new(BodyForm::Quoted(SExp::Atom(v[0].loc(), vec![4]))),
+                            Rc::new(make_operator2(
+                                &v[0].loc(),
+                                "c".to_string(),
+                                Rc::new(BodyForm::Value(SExp::Atom(v[0].loc(), vec![1]))),
+                                captures.clone()
+                            )),
+                            Rc::new(BodyForm::Quoted(SExp::Atom(v[0].loc(), vec![1]))),
+                        ]
+                    ))
+                ]
+            );
+
+        eprintln!("lambda output {}", lambda_output.to_sexp());
+        Ok(lambda_output)
+    } else {
+        // No captures
+        let subparse = frontend(opts, &[mod_form_data])?;
+        Ok(BodyForm::Mod(v[0].loc(), subparse))
+    }
+}
+
 pub fn compile_bodyform(
     opts: Rc<dyn CompilerOpts>,
     body: Rc<SExp>,
@@ -329,6 +440,8 @@ pub fn compile_bodyform(
                             } else if *atom_name == "mod".as_bytes().to_vec() {
                                 let subparse = frontend(opts, &[body.clone()])?;
                                 Ok(BodyForm::Mod(op.loc(), subparse))
+                            } else if *atom_name == "lambda".as_bytes().to_vec() {
+                                handle_lambda(opts, &v)
                             } else {
                                 application()
                             }
